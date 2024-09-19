47ad96f3107ebfad6c2c09c3a96f86f962a669d7
UeritonAraujo <ueritonaraujo160@gmail.com>
2024-09-19 16:36:10 -0300

    Order, Instant, ISO 8601 - Part 2

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,6 +23,8 @@ public class Order implements Serializable{
 	@Id															// Para dizer que será uma tabela do banco de dados
 	@GeneratedValue(strategy = GenerationType.IDENTITY)			// Para dizer que será uma tabela do banco de dados
 	private Long id;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
 	@ManyToOne													// Anotação p/ apontar no bando de dados que será uma associação com muitos para um (cliente com muitos pedidos)
