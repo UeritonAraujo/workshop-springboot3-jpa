46ecc6effd10ce9e508589b05a9d228fdf0c49fe
UeritonAraujo <ueritonaraujo160@gmail.com>
2024-09-20 21:59:54 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -34,6 +37,9 @@ public class Product implements Serializable{
 	joinColumns = @JoinColumn(name = "product_id"),
 	inverseJoinColumns = @JoinColumn(name = "category_id"))														// Nome das tableas e chaves extrangeiras
 	private Set<Category> categories = new HashSet<>();									// Product pd ter várias Categories. Set representa um conjunto: garante q ñ vou ter mais de uma ocorrência na mesma categoria.
+	
+	@OneToMany(mappedBy = "id.product")							// id.order -> pq no orderItem tem id
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product() {
 	}
@@ -89,6 +95,15 @@ public class Product implements Serializable{
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
